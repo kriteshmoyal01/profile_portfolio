6c9583e02a10680ae35926ef90c19254bc4ecb7e
kriteshmoyal01 <kartikdewani01@gmail.com>
2024-11-10 23:18:32 +0530

    service page completed

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
-const Resume = () => {
+// About data
+const about = {
+  title: "About me",
+  description: "I'm a passionate developer with a keen interest in building efficient, scalable web applications. Skilled in modern web technologies, I enjoy creating solutions that make a real impact. Always eager to learn and improve, I'm focused on developing both my technical skills and collaborative capabilities."
+};
+
+// Personal information
+const info = [
+  { fieldName: "Name", fieldValue: "Kritesh Moyal" },
+  { fieldName: "Email", fieldValue: "kartikdewani01@gmail.com" },
+  { fieldName: "Phone", fieldValue: "9571135533" },
+  { fieldName: "Address", fieldValue: "NIT Raipur, Raipur, Chhattisgarh" },
+  { fieldName: "Profession", fieldValue: "Senior Software Development Engineer" },
+  { fieldName: "Company", fieldValue: "Bioinformatics Company" }
+];
+
+
+// Education data
+const education = [
+  { degree: "Bachelor's of Technology (B.Tech.)", field: "Computer Science & Engineering", institution: "NIT Raipur", year: "2017-2021" },
+  { degree: "Master's of Technology (M.Tech.)", field: "Bioinformatics", institution: "Bioinformatics Institute", year: "2021-2023" }
+];
+
+// Experience data
+  
+// About component
+const Resume  = () => {
   return (
     <div>
-      resume page
-    </div>
+      resume page 
+  </div>
   )
-}
-
-export default Resume
+};
+export default Resume  ;
